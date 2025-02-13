8c7600e13bcc126f056033c47db843c191c644f5
diegoddb <diegodiezdebonilla@gmail.com>
2025-02-13 15:01:17 -0800

    Update Base3ToBinary.cpp

--- Base3ToBinary.cpp
+++ Base3ToBinary.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>  // For reverse()
 
 using namespace std;
 
@@ -14,18 +15,18 @@ vector<int> decimalToBase3(int n) {
     return base3;
 }
 
-// **Convert Base-3 Representation to Binary Encoding**
-void base3ToBinaryEncoding(int n) {
+// **Convert Base-3 Representation to Sparse Binary Encoding**
+void base3ToSparseBinary(int n) {
     vector<int> base3 = decimalToBase3(n);
 
     int len = base3.size();
-    vector<int> nonzeroBits(len, 0);
+    vector<int> onesBits(len, 0);
     vector<int> twosBits(len, 0);
 
-    // Construct binary encoding
+    // Construct sparse binary encoding
     for (int i = 0; i < len; i++) {
-        if (base3[i] > 0) nonzeroBits[i] = 1;  // Mark all nonzero digits
-        if (base3[i] == 2) twosBits[i] = 1;    // Mark digits that are exactly 2
+        if (base3[i] == 1) onesBits[i] = 1;  // Mark only 1s
+        if (base3[i] == 2) twosBits[i] = 1;  // Mark only 2s
     }
 
     // Print Base-3 Representation
@@ -33,9 +34,9 @@ void base3ToBinaryEncoding(int n) {
     for (int digit : base3) cout << digit;
     cout << endl;
 
-    // Print Encoded Binary
+    // Print Binary Encoding
     cout << "Binary Encoding: ";
-    for (int bit : nonzeroBits) cout << bit;
+    for (int bit : onesBits) cout << bit;
     cout << " ";
     for (int bit : twosBits) cout << bit;
     cout << endl;
@@ -47,7 +48,7 @@ int main() {
     cout << "Enter a number: ";
     cin >> N;
 
-    base3ToBinaryEncoding(N);
+    base3ToSparseBinary(N);
 
     return 0;
 }
